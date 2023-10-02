4d2f0aa81d14173192e2645a08ebcd3b077c5877
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2023-10-02 12:35:31 +0530

    Update ResultDialog.java

--- app/src/main/java/com/lovishprabhakar/tictactoe/ResultDialog.java
+++ app/src/main/java/com/lovishprabhakar/tictactoe/ResultDialog.java
@@ -1,21 +1,24 @@
 package com.lovishprabhakar.tictactoe;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
 public class ResultDialog extends Dialog {
     private final String message;
     private final MainActivity mainActivity;
+
     public ResultDialog(@NonNull Context context, String message, MainActivity mainActivity) {
         super(context);
         this.message = message;
         this.mainActivity = mainActivity;
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
